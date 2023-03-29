78bbbd793ca19ca021835f7e7bdc234b5f56b71d
Spuzzelsnest <j.mpdesmet@protonmail.com>
2023-03-30 01:26:55 +0200

    added popular ports - removed over verbose colors

--- psNmap.ps1
+++ psNmap.ps1
@@ -29,7 +29,7 @@ $TCPports = @(20,21,22,23,53,80,443,445,3389,4444,8080)
 $ips = @()
 $openPorts = @{}
 
-Write-Host $ifList.count "Networkadapter(s) found.`n"$ifList[0].Name"with IP $ownIP `nRunnig ping sweep on range "$ipRange"1/24"
+Write-Host $ifList.count "Networkadapter(s) found.`n-"$ifList[0].Name"with IP $ownIP `nRunnig ping sweep on range "$ipRange"1/24"
 Clear-Content -Path .\hostname.txt
 
 # Running ping scan on default range from 1 to 254 
@@ -42,25 +42,25 @@ For ($i = 1; $i -lt 254; $i++) {
         if ( Test-Connection -count 1 -comp $ip -quiet ){
             $ips += $ip
         }
-    } 
+    }
 }
 
-"... Starting port scan on $TCPports ..."
+Write-Host "... Starting scanning for popular ports ...`n[$TCPports] on "$ips.Count" host(s)"
 
 foreach ($ip in $ips){
-    write-host "found:  $ip"
     foreach ($TCPport in $TCPports){
         If(Test-Connection -BufferSize 32 -Count 1 -Quiet -ComputerName $ip){
             $socket = New-Object System.Net.Sockets.TcpClient($ip, $TCPport)
             If($socket.Connected) {
-                Write-Host "$ip port $TCPport open" -ForegroundColor Green
                 $openPorts.Add($ip,$TCPport) 
                 $socket.Close()
             }else{
-                Write-Host "$ip port $TCPport not open" -ForegroundColor Red
+                Write-Debug "$ip port $TCPport not open"
             }
         }
     }
 }
+
+Write-host "`nFound the folling open ports" -ForegroundColor Green
 
 $openPorts
