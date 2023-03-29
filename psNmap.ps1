ec40acd52de96c4b1fb518f0dea69e8dd75c986d
Spuzzelsnest <j.mpdesmet@protonmail.com>
2023-03-29 23:21:23 +0200

    added portscan with deafult port

--- psNmap.ps1
+++ psNmap.ps1
@@ -3,7 +3,7 @@
     Portscan that prints the ping responce to a text file: hostname.txt
 
 .DESCRIPTION
-    The Portscan is used to scan a r
+    The Portscan is used to scan a range of ports
  
 .NOTES
     Run in default powershell.
@@ -18,13 +18,19 @@
     $ip     - is used to define the first 3 octets of the IP range wich is used to run the script. It searches from 1 to 254 via a ping scan through the IP addresses.
 #>
 
-$ifList = Get-NetAdapter | ?{ $_.Status -eq "Up" }
+# env
+$ErrorActionPreference = "silentlycontinue"
 
+# Vars
+$ifList = Get-NetAdapter | Where-Object { $_.Status -eq "Up" }
 $ownIP = (Get-NetIPAddress -InterfaceIndex $ifList[0].ifIndex).IPAddress
-$ipRange = (($ownIP.Split(".")|select -First 3) -join ".")+"."
-Write-Host $ifList.count " Networkadapter(s) found.`n"$ifList[0].Name" with IP $ownIP `nRunnig ping sweep on range "$ipRange"1/24`n"
+$ipRange = (($ownIP.Split(".")|Select-Object -First 3) -join ".")+"."
+$port = 22
+
+Write-Host $ifList.count "Networkadapter(s) found.`n"$ifList[0].Name"with IP $ownIP `nRunnig ping sweep on range "$ipRange"1/24"
 Clear-Content -Path .\hostname.txt
-For ($i = 1; $i -lt 254; $i++) {
+
+For ($i = 50; $i -lt 70; $i++) {
 
             $ip = $ipRange+$i
 
@@ -33,5 +39,20 @@ For ($i = 1; $i -lt 254; $i++) {
             } 
 }
 
-write-host "Scann donen content of hostname.txt"
-Get-Content .\hostname.txt
+"Scan done content of hostname.txt`n...Starting port scan ..."
+
+$ips = Get-Content .\hostname.txt
+
+foreach ($ip in $ips){
+    write-host "found:  $ip"
+    If(Test-Connection -BufferSize 32 -Count 1 -Quiet -ComputerName $ip){
+        $socket = New-Object System.Net.Sockets.TcpClient($ip, $port)
+        If($socket.Connected) {
+            "$ip port $port open"
+            $socket.Close()
+        }else{
+            
+            "$ip port $port not open"
+        }
+    }
+}
