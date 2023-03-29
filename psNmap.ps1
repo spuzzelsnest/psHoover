8855f685ed98da30fc58ac1663387787015e9ca5
Spuzzelsnest <j.mpdesmet@protonmail.com>
2023-03-29 23:33:19 +0200

    added 3 default port 22,80,443

--- psNmap.ps1
+++ psNmap.ps1
@@ -25,34 +25,34 @@ $ErrorActionPreference = "silentlycontinue"
 $ifList = Get-NetAdapter | Where-Object { $_.Status -eq "Up" }
 $ownIP = (Get-NetIPAddress -InterfaceIndex $ifList[0].ifIndex).IPAddress
 $ipRange = (($ownIP.Split(".")|Select-Object -First 3) -join ".")+"."
-$port = 22
+$ports = @(22,80,443)
+$ips = @()
 
 Write-Host $ifList.count "Networkadapter(s) found.`n"$ifList[0].Name"with IP $ownIP `nRunnig ping sweep on range "$ipRange"1/24"
 Clear-Content -Path .\hostname.txt
 
-For ($i = 50; $i -lt 70; $i++) {
+# Running ping scan on default range from 1 to 254 
 
-            $ip = $ipRange+$i
-
-            if( Test-Connection -count 1 -comp $ip -quiet ){
-                $ip | Out-File -Append -FilePath .\hostname.txt
-            } 
+For ($i = 1; $i -lt 254; $i++) {
+    $ip = $ipRange+$i
+    if( Test-Connection -count 1 -comp $ip -quiet ){
+        $ips += ,$ip
+    } 
 }
 
-"Scan done content of hostname.txt`n...Starting port scan ..."
-
-$ips = Get-Content .\hostname.txt
+"Scan done content of hostname.txt`n...Starting port scan on $ports ..."
 
 foreach ($ip in $ips){
     write-host "found:  $ip"
-    If(Test-Connection -BufferSize 32 -Count 1 -Quiet -ComputerName $ip){
-        $socket = New-Object System.Net.Sockets.TcpClient($ip, $port)
-        If($socket.Connected) {
-            "$ip port $port open"
-            $socket.Close()
-        }else{
-            
-            "$ip port $port not open"
+    foreach ($port in $ports){
+        If(Test-Connection -BufferSize 32 -Count 1 -Quiet -ComputerName $ip){
+            $socket = New-Object System.Net.Sockets.TcpClient($ip, $port)
+            If($socket.Connected) {
+                Write-Host "$ip port $port open" -ForegroundColor Green
+                $socket.Close()
+            }else{
+                write-host "$ip port $port not open" -ForegroundColor Red
+            }
         }
     }
 }
